f5c8d36d878524564dd9f20cdf6c2339f0172669
Your Name <rehanjananga2015@gmail.com>
2019-09-09 14:23:44 +0530

    task 2 without regid and name

--- mysql-db-create.sh
+++ mysql-db-create.sh
@@ -1,22 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "reg_id"
-read reg_id
-echo "name"
-read name
-echo "subject"
-read subject
-echo " marks"
-read  marks
+mysql -u root -p"" << EOF
 
+use school;
 
-
-mysql -u root -ppassword school << EOF
-INSERT INTO student(reg_id,name,subject,marks) VALUES ("$reg_id" ,"$name", "$subject" , "$marks");
-	
 EOF
 
- 
+echo -n "Enter numebr of Students to enroll"
 
+read i
 
+echo -n "Enter marks"
 
+read marks
+
+for (( i=1; i‹= 10; i++ ))
+
+do   
+
+for((sub=1; sub‹= 8; sub++ ))
+
+do
+
+echo "Enter subject name"
+
+read subject
+
+if [$marks=y | y<=100 | y>=0] then
+
+<< EOF
+
+insert into student('reg_id$i',$subject,$marks)
+
+EOF
+
+echo Record Inserted
+
+done
+
+done
